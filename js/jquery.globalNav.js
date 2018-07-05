d5be8182dd76a6d72184c56c7da06580cd52f13c
abenosan00 <arekusisu00@yahoo.co.jp>
2018-07-05 12:54:18 +0900

    style(nav.css,index.html)添削部分の修正をしました。

--- js/jquery.globalNav.js
+++ js/jquery.globalNav.js
@@ -1,13 +1,14 @@
-(function($) {
-    $(function() {
-        var $header = $('header');
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > 50) {
-                $header.addClass('fixed');
-            } else {
-                $header.removeClass('fixed');
-            }
+
+    (function($) {
+        $(function() {
+            var $header = $('header');
+            $(window).scroll(function() {
+                if ($(window).scrollTop() > 50) {
+                    $header.addClass('fixed');
+                } else {
+                    $header.removeClass('fixed');
+                }
+            });
         });
-    });
-})(jQuery);
+    })(jQuery);
 
