bb22476ee26d38c6feef146f2e5e822f6d8c1a04
abenosan00 <arekusisu00@yahoo.co.jp>
2018-07-05 12:18:11 +0900

    s

---
+++ js/jquery.globalNav.js
@@ -0,0 +1,13 @@
+(function($) {
+    $(function() {
+        var $header = $('header');
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 50) {
+                $header.addClass('fixed');
+            } else {
+                $header.removeClass('fixed');
+            }
+        });
+    });
+})(jQuery);
+
