9c4fcb12851b1de67d635b95efba60ef95f4a55e
abenosan00 <arekusisu00@yahoo.co.jp>
2018-07-05 15:36:36 +0900

    style(jquery.header.js)添削部分の修正をしました。

--- js/jquery.header.js
+++ js/jquery.header.js
@@ -5,17 +5,15 @@
 (function($) {
     $(function() {
         var header = $('header');
-        /* メニューバーの切り替え */
-        var fixedClassName = $('fixed');
-        /* 縦のサイズ 200以上なると実行 */
+        /* 縦のサイズ 25以上なると実行 */
         const fixedBorder = 25;
 
-        /* 縦幅が40%以下になったらfixedの処理をする */
+        /* 縦幅が25以下になったらfixedの処理をする */
         $(window).scroll(function() {
             if ($(window).scrollTop() > fixedBorder) {
-                header.addClass(fixedClassName);
+                header.addClass('fixed');
             } else {
-                header.removeClass(fixedClassName);
+                header.removeClass('fixed');
             }
         });
     });
