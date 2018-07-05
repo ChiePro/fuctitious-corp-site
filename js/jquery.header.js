a25098528d810aeabf488d4e7581a5eaa3dc7aae
abenosan00 <arekusisu00@yahoo.co.jp>
2018-07-05 15:23:43 +0900

    s

--- js/jquery.header.js
+++ js/jquery.header.js
@@ -2,18 +2,20 @@
  * Header JS
  */
 
-/* 横幅が40%以下になったらfixedの処理をする */
 (function($) {
     $(function() {
         var header = $('header');
-        var fixed = $('fixed');
+        /* メニューバーの切り替え */
+        var fixedClassName = $('fixed');
         /* 縦のサイズ 200以上なると実行 */
-        const number = 200;
+        const fixedBorder = 25;
+
+        /* 縦幅が40%以下になったらfixedの処理をする */
         $(window).scroll(function() {
-            if ($(window).scrollTop() > number) {
-                header.addClass(fixed);
+            if ($(window).scrollTop() > fixedBorder) {
+                header.addClass(fixedClassName);
             } else {
-                header.removeClass(fixed);
+                header.removeClass(fixedClassName);
             }
         });
     });
