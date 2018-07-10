9fe8a3802c153f720cc61c8b3489a7b699169a47
abenosan00 <arekusisu00@yahoo.co.jp>
2018-07-10 15:59:25 +0900

    style(jquery.header.js)メニューバーの調整

--- js/jquery.header.js
+++ js/jquery.header.js
@@ -7,9 +7,9 @@
         const header = $('header');
         const fixedClassName ='fixed';
         /* メニューバー の表示微調整 */
-        const fixedCodic = 20 ;
+        const fixed_minor_adjustment = 20 ;
         /* この値以上スクロールすると header を固定する */
-        const fixedBorder = $('#top_image_slider').outerHeight(true) - fixedCodic;
+        const fixedBorder = $('#top_image_slider').outerHeight(true) - fixed_minor_adjustment;
 
         /* fixedBorder の値以上スクロールしたら */
         $(window).scroll(function() {
